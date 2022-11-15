f52b0a562e47a4c9b9c686ec2f4b3c9bb436ce06
ajay <ajay.shanmuga.tommie@gmail.com>
2022-11-15 19:44:37 +0100

    leapfrog complete

--- homework 3/leapfrog.py
+++ homework 3/leapfrog.py
@@ -1,8 +1,9 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-def leapfrog(p, phi, nmd, beta = 1, jn = 1, h = 1): #p and phi are some initial values
+def leapfrog(p, phi, nmd, N = 20, beta = 1, jn = 1, h = 1): #p and phi are some initial values
     #nmd is the number of steps
-    #beta, jn, h are parameters of the system
+    #N, beta, jn, h are parameters of the system
     eps = 1/nmd
     pi, phhi = p, phi
     #first step
@@ -15,3 +16,26 @@ def leapfrog(p, phi, nmd, beta = 1, jn = 1, h = 1): #p and phi are some initial 
     pi = pi - eps*(phhi/(beta*jn) - N*np.tanh(beta*h + phhi))
     phhi = phhi + 0.5*eps*pi
     return pi, phhi #p-f, phi-f values
+
+def hamlf(p, phi, N = 20, beta = 1, jn = 1, h = 1): #function to calculate the energy of a given configuration
+    return 0.5*p*p + 0.5*phi*phi/(beta*jn) - N*np.log(2*np.cosh(beta*h + phi))
+
+p, phi = 1, 1 #fixed init values
+#p, phi = np.random.rand(), np.random.rand() #random init values
+H_old = hamlf(p, phi) #energy of the init configuration
+H_new = np.zeros(100) #array to store the energies of the final config
+for i in range(100):
+    p2, phi2 = leapfrog(p, phi, i+1)
+    H_new[i] = hamlf(p2, phi2)
+
+xrange = np.arange(100) + 1
+yval = np.abs((H_new - H_old)/H_old)
+
+#plotting
+plt.plot(xrange, yval, '.')
+plt.yscale('log')
+plt.xlabel('MD steps')
+plt.ylabel('$\\vert\\frac{\\mathcal{H}[p_f, \\phi_f] - \\mathcal{H}[p_i, \\phi_i]}{\\mathcal{H}[p_i, \\phi_i]}\\vert$')
+plt.title('Relative error vs. MD steps')
+plt.grid()
+plt.show()
