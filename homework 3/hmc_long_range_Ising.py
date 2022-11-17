ea9913b66095e7dbe99f6ddde2e1ce605c81892f
dongjinsuh <djsuh98@gmail.com>
2022-11-17 02:48:48 +0100

    atempt to fix problems

--- homework 3/hmc_long_range_Ising.py
+++ homework 3/hmc_long_range_Ising.py
@@ -29,14 +29,14 @@ def leapfrog(p, phi, nmd, N = 20, jn = 1, h = 1): #p and phi are some initial va
 def HMC_alg(phi0,J,h,N,nmd,N_samples,N_burn):
     #phi = 1
     acceptence = 0
-    phi_all=[]
+    phi_all=np.zeros((N_samples))
     mu = 0
     sig = 1
-    
+    phi = phi0
     for i in range(N_burn):
         p0 = np.random.normal(mu, sig)
-        p_leap, phi_leap = leapfrog(p0, phi0, nmd, N, J, h)
-        H_old = hamlf(p0, phi0, N, J, h)
+        p_leap, phi_leap = leapfrog(p0, phi, nmd, N, J, h)
+        H_old = hamlf(p0, phi, N, J, h)
         H_new = hamlf(p_leap,phi_leap, N, J, h)
         delta_E = H_old - H_new
         random=np.random.rand()
@@ -44,13 +44,12 @@ def HMC_alg(phi0,J,h,N,nmd,N_samples,N_burn):
         if random < np.exp(-delta_E):
             phi = phi_leap
         else:
-            phi = phi0
+            phi = phi
     
     for i in range(N_samples):
         p0 = np.random.normal(mu, sig)
-        p_leap, phi_leap = leapfrog(p0, phi0, nmd, N, J, h)
-    
-        H_old = hamlf(p0, phi0, N, J, h)
+        p_leap, phi_leap = leapfrog(p0, phi, nmd, N, J, h)
+        H_old = hamlf(p0, phi, N, J, h)
         H_new = hamlf(p_leap,phi_leap, N, J, h)
         delta_E = H_old - H_new
         random=np.random.rand()
@@ -59,7 +58,7 @@ def HMC_alg(phi0,J,h,N,nmd,N_samples,N_burn):
             phi = phi_leap
             acceptence += 1
         else:
-            phi = phi0
+            phi = phi
         
         phi_all[i] = phi
     
@@ -72,12 +71,12 @@ def magnetization_obs(phi, h):
 
 def energy_obs(phi, N, h, J):
     beta = 1
-    return - np.power(phi,2)/(2*N*J*np.power(beta,2)) + h*np.tanh(beta*h+phi)
+    return - np.power(phi,2)/(2*N*J*np.power(beta,2)) - h*np.tanh(beta*h + phi)
 
 
 phi0 = 1 # starting initial phi
 h = 0.5
-J = np.arange(.2,2,0.1)
+J = np.arange(0.2,2,0.1)
 N = [5,10,15]   # number of sites 
 nmd = 5
 N_samples = 10000
@@ -86,10 +85,10 @@ N_burn = 300
 
 len_N = len(N)
 len_J = len(J)
-accept_rate = np.zeros(len_N,len_J)
+accept_rate = np.zeros((len_N,len_J))
 #phi_mc = np.zeros( len_N,len_J, N_samples)
-energy = np.zeros( len_N,len_J, N_samples)
-magnet = np.zeros( len_N,len_J, N_samples)
+energy = np.zeros( (len_N,len_J, N_samples))
+magnet = np.zeros( (len_N,len_J, N_samples))
 
 
 for m,n in enumerate(N):
@@ -97,7 +96,7 @@ for m,n in enumerate(N):
         x = HMC_alg(phi0,j,h,n,nmd,N_samples,N_burn)
         phi_mc = x[0]
         accept_rate[m,i] = x[1]
-        #print()
+        print(n,j,x[1])
 
         magnet[m,i,:] = magnetization_obs(phi_mc, h)
         energy[m,i,:] = energy_obs(phi_mc,n,h,j)
@@ -106,26 +105,26 @@ for m,n in enumerate(N):
 # Use bootstrap method to calculate the errors
 
 num_bs = 100
-mag_bs = np.zeros(len_N, len_J, num_bs)
-energy_bs = np.zeros(len_N, len_J, num_bs)
+mag_bs = np.zeros((len_N, len_J, num_bs))
+energy_bs = np.zeros((len_N, len_J, num_bs))
 
 for b in range(num_bs):
     # choose new random configuration of the markov chain
     bs_config = np.random.randint(0,N_samples, size=N_samples)
-    mag_bs[:,:,b] = np.mean(magnet[:,:,bs_config])
-    energy_bs[:,:,b] = np.mean(energy[:,:,bs_config])
+    mag_bs[:,:,b] = np.mean(magnet[:,:,bs_config], axis = -1)
+    energy_bs[:,:,b] = np.mean(energy[:,:,bs_config], axis= -1)
 
 # get the average energy and mean magnetization per site and their errors of bootstrap
-avg_mag = np.mean(mag_bs) 
-avg_ener = np.mean(energy_bs)
-avg_mag_err = np.std(mag_bs)
-avg_ener_err = np.std(energy_bs)
+avg_mag = np.mean(mag_bs, axis= -1) 
+avg_ener = np.mean(energy_bs, axis= -1)
+avg_mag_err = np.std(mag_bs, axis= -1)
+avg_ener_err = np.std(energy_bs, axis= -1)
 
 
 fig = plt.figure(figsize=(10,7.5))
 
 for n,n_val in enumerate(N):
-    plt.errorbar(1/J, avg_mag[n:], avg_mag_err[n:], fmt='s', color='blue', capthick=1, label='N='+str(n_val))
+    plt.errorbar(J, avg_mag[n,:], avg_mag_err[n,:], fmt='.', capthick=1, label='N='+str(n_val))
 
 plt.xlabel('J')
 plt.ylabel('mean magnetization <m>')
@@ -134,7 +133,7 @@ plt.show()
 
 
 for n,n_val in enumerate(N):
-    plt.errorbar(1/J, avg_ener[n:], avg_ener_err[n:], fmt='s', color='blue', capthick=1,label='N='+str(n_val))
+    plt.errorbar(J, avg_ener[n,:], avg_ener_err[n,:], fmt='.', capthick=1,label='N='+str(n_val))
 
 plt.xlabel('J')
 plt.ylabel('average energy <e>')
