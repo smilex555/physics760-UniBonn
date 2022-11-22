fd3c10b61944002b4fdf38a466c33f6bdc332844
ajay <ajay.shanmuga.tommie@gmail.com>
2022-11-22 19:38:50 +0100

    leapfrog update --not working

--- homework 4/leapfrog.py
+++ homework 4/leapfrog.py
@@ -1,32 +1,39 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def leapfrog(p, phi, nmd, N = 20, beta = 1, jn = 1, h = 1): #p and phi are some initial values
-    #nmd is the number of steps
-    #N, beta, jn, h are parameters of the system
-    eps = 1/nmd
-    pi, phhi = p, phi
-    #first step
-    phhi = phhi + 0.5*eps*pi
-    #intermediate steps
-    for i in range(nmd - 1):
-        pi = pi - eps*(phhi/(beta*jn) - N*np.tanh(beta*h + phhi))
-        phhi = phhi + eps*pi
+def leapfrog(p, phi, mpik, fk, deltafk, nmd, tau = 1, beta = 1000):
+    eps = tau/nmd
+    fpi, fphi = p, phi
+    # first step
+    fphi += .5*eps*fpi
+    # intermediate steps
+    for _ in range(nmd - 1):
+        for i in range(len(phi)):
+            fpi[i] += eps*beta*(np.sum((fk - fitfun(mpik, fphi))*(mpik**i)/(deltafk*deltafk)))
+        fphi += eps*fpi
     #last step
-    pi = pi - eps*(phhi/(beta*jn) - N*np.tanh(beta*h + phhi))
-    phhi = phhi + 0.5*eps*pi
-    return pi, phhi #p-f, phi-f values
+    for i in range(len(phi)):
+        fpi[i] += eps*beta*(np.sum((fk - fitfun(mpik, fphi))*(mpik**i)/(deltafk*deltafk)))
+    fphi += .5*eps*fpi
+    return fpi, fphi
 
-def hamlf(p, phi, N = 20, beta = 1, jn = 1, h = 1): #function to calculate the energy of a given configuration
-    return 0.5*p*p + 0.5*phi*phi/(beta*jn) - N*np.log(2*np.cosh(beta*h + phi))
+def ham(p, phi, mpik, fk, deltafk, beta = 1000):
+    return np.sum(0.5*p*p) + beta*0.5*np.sum((fk - fitfun(mpik, phi))*(fk - fitfun(mpik, phi))/(deltafk*deltafk))
 
-p, phi = 1, 1 #fixed init values
-#p, phi = np.random.rand(), np.random.rand() #random init values
-H_old = hamlf(p, phi) #energy of the init configuration
+def fitfun(mpi, x):
+    return x[0] + x[1]*mpi + x[2]*mpi*mpi
+
+mpik = np.array([.176, .234, .260, .284, .324])
+fk = np.array([960., 1025., 1055., 1085., 1130.])
+deltafk = np.array([25., 20., 15., 10., 8.])
+
+p, phi = np.array([1., 1., 1.]), np.array([800., 800., 600.]) #fixed init values
+#p, phi = np.array([np.random.rand(), np.random.rand(), np.random.rand()]), np.array([np.random.rand(), np.random.rand(), np.random.rand()]) #random init values
+H_old = ham(p, phi, mpik, fk, deltafk) #energy of the init configuration
 H_new = np.zeros(100) #array to store the energies of the final config
 for i in range(100):
-    p2, phi2 = leapfrog(p, phi, i+1)
-    H_new[i] = hamlf(p2, phi2)
+    p2, phi2 = leapfrog(p, phi, mpik, fk, deltafk, i+1)
+    H_new[i] = ham(p2, phi2, mpik, fk, deltafk)
 
 xrange = np.arange(100) + 1
 yval = np.abs((H_new - H_old)/H_old)
