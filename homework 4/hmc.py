5eff83e1e911869f25b005411d884eac440bc77b
ajay <ajay.shanmuga.tommie@gmail.com>
2022-11-24 17:02:03 +0100

    edit

--- homework 4/hmc.py
+++ homework 4/hmc.py
@@ -4,7 +4,9 @@ from leapfrog import leapfrog, ham
 
 def HMC(phi0, hmcmpik, hmcfk, hmcdeltafk, hmcnmd, hmcn_samples, hmcn_burn, tau = 1, beta = 1000):
     acceptance = 0
-    phi_all = np.zeros((hmcn_samples))
+    phi_all0 = np.zeros((hmcn_samples))
+    phi_all1 = np.zeros((hmcn_samples))
+    phi_all2 = np.zeros((hmcn_samples))
     phi = phi0.copy()
     for _ in range(hmcn_burn):
         p0 = np.random.normal(1, 0)
@@ -29,4 +31,14 @@ def HMC(phi0, hmcmpik, hmcfk, hmcdeltafk, hmcnmd, hmcn_samples, hmcn_burn, tau =
         else:
             phi = phi
 
-    return phi_all, acceptance/hmcn_samples
+        phi_all0[i] = phi[0]
+        phi_all1[i] = phi[1]
+        phi_all2[i] = phi[2]
+
+    return phi_all0, phi_all1, phi_all2, acceptance/hmcn_samples
+
+mpik = np.array([.176, .234, .260, .284, .324])
+fk = np.array([960., 1025., 1055., 1085., 1130.])
+deltafk = np.array([25., 20., 15., 10., 8.])
+
+phidata = np.array([800., 800., 600.])
