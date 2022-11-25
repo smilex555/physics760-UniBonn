f9d640dd25facf29c1f7d3ebf220aadc58267f0d
dongjinsuh <djsuh98@gmail.com>
2022-11-25 12:27:01 +0100

    fix problem at calculating H_new

--- homework 4/hmc.py
+++ homework 4/hmc.py
@@ -1,9 +1,32 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from leapfrog import leapfrog, ham, fitfun
+#from leapfrog import leapfrog, ham, fitfun
 
 
-def HMC(phi0, hmcmpik, hmcfk, hmcdeltafk, hmcnmd, hmcn_samples, hmcn_burn, tau = 1, beta = 1000):
+def leapfrog(p, phi, mpik, fk, deltafk, nmd, tau, beta = 1000):
+    eps = tau/nmd
+    fpi, fphi = p.copy(), phi.copy()
+    # first step
+    fphi += .5*eps*fpi
+    # intermediate steps
+    for _ in range(nmd - 1):
+        for i in range(len(phi)):
+            fpi[i] += eps*beta*(np.sum((fk - fitfun(mpik, fphi))*(mpik**i)/(deltafk*deltafk)))
+        fphi += eps*fpi
+    # last step
+    for i in range(len(phi)):
+        fpi[i] += eps*beta*(np.sum((fk - fitfun(mpik, fphi))*(mpik**i)/(deltafk*deltafk)))
+    fphi += .5*eps*fpi
+    return fpi, fphi
+
+def ham(p, phi, mpik, fk, deltafk, beta = 1000):
+    return np.sum(0.5*p*p) + beta*0.5*np.sum((fk - fitfun(mpik, phi))*(fk - fitfun(mpik, phi))/(deltafk*deltafk))
+
+def fitfun(mpi, x):
+    return x[0] + x[1]*mpi + x[2]*mpi*mpi
+
+
+def HMC(phi0, hmcmpik, hmcfk, hmcdeltafk, hmcnmd, hmcn_samples, hmcn_burn, tau, beta = 1000):
     acceptance = 0
     phi_all0 = np.zeros((hmcn_samples))
     phi_all1 = np.zeros((hmcn_samples))
@@ -11,10 +34,10 @@ def HMC(phi0, hmcmpik, hmcfk, hmcdeltafk, hmcnmd, hmcn_samples, hmcn_burn, tau =
     phi = phi0.copy()
     for _ in range(hmcn_burn):
         #p0 = np.random.normal(1, 0)
-        p0 = np.array([np.random.normal(1,0), np.random.normal(1,0), np.random.normal(1,0)])
+        p0 = np.array([np.random.normal(0,1), np.random.normal(0,1), np.random.normal(0,1)])
         p_leap, phi_leap = leapfrog(p0, phi, hmcmpik, hmcfk, hmcdeltafk, hmcnmd, tau, beta)
         H_old = ham(p0, phi, hmcmpik, hmcfk, hmcdeltafk, beta)
-        H_new = ham(p_leap, phi_leap, hmcfk, hmcdeltafk, beta)
+        H_new = ham(p_leap, phi_leap, hmcmpik, hmcfk, hmcdeltafk, beta)
         delta_E = H_new - H_old
         if np.random.rand() < np.exp(-delta_E):
             phi = phi_leap
@@ -23,11 +46,19 @@ def HMC(phi0, hmcmpik, hmcfk, hmcdeltafk, hmcnmd, hmcn_samples, hmcn_burn, tau =
 
     for i in range(hmcn_samples):
         #p0 = np.random.normal(1, 0)
-        p0 = np.array([np.random.normal(1,0), np.random.normal(1,0), np.random.normal(1,0)])
+        p0 = np.array([np.random.normal(0,1), np.random.normal(0,1), np.random.normal(0,1)])
+        #p0 = np.array([np.random.rand(), np.random.rand(), np.random.rand()])
+        #p0 = np.array([1., 1., 1.])
+        #print(p0)
+        #print(phi)
         p_leap, phi_leap = leapfrog(p0, phi, hmcmpik, hmcfk, hmcdeltafk, hmcnmd, tau, beta)
+        #print(p_leap, phi_leap)
         H_old = ham(p0, phi, hmcmpik, hmcfk, hmcdeltafk, beta)
-        H_new = ham(p_leap, phi_leap, hmcfk, hmcdeltafk, beta)
-        delta_E = H_new - H_old
+        #print(H_old)
+        H_new = ham(p_leap, phi_leap, hmcmpik, hmcfk, hmcdeltafk, beta)
+        #print(H_new)
+        delta_E = np.abs((H_new - H_old))
+        #print(delta_E)
         if np.random.rand() < np.exp(-delta_E):
             phi = phi_leap
             acceptance += 1
@@ -46,31 +77,27 @@ deltafk = np.array([25., 20., 15., 10., 8.])
 
 phidata = np.array([800., 800., 600.])
 
-nmd = 100
+nmd = 20
 Tau = 1
-N_samples = 1000000
-N_burn = 100000
+N_samples = 100000
+N_burn = 10000
 
 
 phi_hmc = HMC(phidata, mpik, fk, deltafk, nmd, N_samples, N_burn, Tau)
-
+print(phi_hmc[3])
 # create x values to plot the behavior of φi as a function of HMC trajectory
 hmc_trajectory = np.arange(0, N_samples, 1) 
 
 #calculate the average values of phi_i and their std in Markov chain,
 # which is the best fit values and its fit error
-average_phi0 = np.mean(phi_hmc[0])
-average_phi1 = np.mean(phi_hmc[1])
-average_phi2 = np.mean(phi_hmc[2])
+average_phi = np.mean(phi_hmc)
 
-std_phi0 = np.std(phi_hmc[0])
-std_phi1 = np.std(phi_hmc[1])
-std_phi2 = np.std(phi_hmc[2])
+phi_fit_data = fitfun(mpik, average_phi)
+
+std_phi0 = np.std(phi_hmc)
 
 
-
-
-fig = plt.figure(figsize=(10,7.5))
+plt.figure(figsize=(10,7.5))
 
 plt.plot(hmc_trajectory, phi_hmc[0],'.', label='phi_0')
 plt.plot(hmc_trajectory, phi_hmc[1],'.', label='phi_1')
@@ -81,3 +108,11 @@ plt.grid()
 plt.legend()
 plt.show()
 
+
+plt.errorbar(mpik, fk, deltafk, fmt='.', capthick=1, label='neutron mass data')
+plt.errorbar(mpik, phi_fit_data, 0, fmt='.', capthick=1, label='neutron mass fit')
+plt.xlabel('pion mass')
+plt.ylabel('neutron mass')
+plt.grid()
+plt.legend()
+plt.show()
